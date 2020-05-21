1fc894a8ed3490b2891062a22a4247bc582c0e25
Yagi-G0AT <ofk7p.48325497p@softbank.ne.jp>
2020-05-21 20:30:26 +0900

    コード確認

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="home">
+   <button @click="add">送信</button>
+  </div>
+</template>
+<script>
+import firebase from "firebase";
+export default {
+ methods: {
+   add() {
+     var db = firebase.firestore();
+     db.collection("users")
+       .add({
+         name: "太郎",
+         age: 20
+       })
+       .then(doc => {
+         console.log(doc);
+       });
+   }
+ }
+};
+</script>
